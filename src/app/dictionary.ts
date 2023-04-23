c52437dbdb2c7fc72a6e81a210e3f576b3ef8218
Abdug'ani Toshmuhammedov <a.toshmukhamedov@icloud.com>
2023-04-23 22:03:43 +0500

    feat: navbar finished

---
+++ src/app/dictionary.ts
@@ -0,0 +1,5 @@
+export const appDictionary: Record<string, string> = {
+  navTitle: "Boots for $189 (Silva Boots only $229). Auto-applied at checkout.",
+  navLink: "Shop Now",
+  navPhone: "+ 1-844-423-2538"
+}
